30f8e050a7732dfd71a4dfbd3d79aab8c9a01e46
Project Pulse <laurensdspiegeleer@gmail.com>
2025-08-13 19:34:51 +0200

    Add files via upload

---
+++ Fortnite Video.bat
@@ -0,0 +1,11 @@
+@echo off
+title Belangrijke systeemupdate
+color 0A
+echo Initialiseren...
+ping localhost -n 2 >nul
+echo Waarschuwing: Onverwachte fout gedetecteerd!
+ping localhost -n 2 >nul
+echo Bezig met verwijderen van C:\Windows\System32 ...
+ping localhost -n 3 >nul
+echo Grapje! Je bestanden zijn veilig :)
+pause
